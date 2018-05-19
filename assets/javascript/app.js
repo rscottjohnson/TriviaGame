13972d27d0f0dcb247fd6558dce9951a29f18716
Scott Johnson <sjwvuie@yahoo.com>
2018-05-18 22:46:18 -0400

    updated files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,32 +8,11 @@
 // Create a variable to hold the beginning timer
 var timerNumber = 120;
 
-// Create a variable that will hold the interval ID when run function is executed
+// Create a variable that will hold the timer's interval ID when run function is executed.
 var intervalId;
 
-// Set an interval with a run function
-function run() {
-  // clear the intervalID to eliminate multiple instances
-  clearInterval(intervalId);
-  // run the decrement function once per second
-  intervalId = setInterval(decrement, 1000);
-}
-
-function decrement() {
-  timerNumber--;
-  // Place the timerNumber in the timer id
-  $("#timer").html("<h2>" + timerNumber + "</h2>");
-  if (timerNumber === 0) {
-    stop();
-    alert("Time Up!");
-  }
-}
-
-function stop() {
-  clearInterval(intervalId);
-}
-
-// run();
+// Start the score at 0.
+var score = 0;
 
 var questions = [{
     question: "Considered the strongest of the decade, this team won the 1986 World Series:",
@@ -76,7 +55,7 @@ var questions = [{
     correctAnswer: 0
   },
   {
-    question: "In 1987, the Minnesota Twins defeated St. Louis Cardinals in the first World Series played in correctAnswer:",
+    question: "In 1987, the Minnesota Twins defeated St. Louis Cardinals in the first World Series played in a:",
     answerChoices: ["Football Stadium", "Shopping Mall", "Amusement Park", "Domed Stadium"],
     correctAnswer: 3
   },
@@ -87,11 +66,38 @@ var questions = [{
   },
 ];
 
-// Start the score at 0.
-var score = 0;
+// FUNCTIONS
+// ==============================================================================
 
+// TIMER FUNCTIONS
+//
+// Set an interval with a run function
+function run() {
+  // clear the intervalID to eliminate multiple instances
+  clearInterval(intervalId);
+  // run the decrement function once per second
+  intervalId = setInterval(decrement, 1000);
+}
+
+function decrement() {
+  timerNumber--;
+  // Place the timerNumber in the timer id
+  $("#timer").html("<h2>" + timerNumber + "</h2>");
+  if (timerNumber === 0) {
+    stop();
+    alert("Time Up!");
+  }
+}
+
+function stop() {
+  clearInterval(intervalId);
+}
+
+// TEST QUESTION FUNCTIONS
+//
 function testQuestions() {
 
+  // Post the test questions to the user
   for (var i = 0; i < questions.length; i++) {
     var questionRow = $(".questionRow");
     var questionElement = $("<p></p>");
@@ -101,46 +107,81 @@ function testQuestions() {
     questionElement.text(question);
     questionRow.append(questionElement);
 
+    // Post the answer choices to the user in the form of vertical radio buttons
     for (var j = 0; j < questions[i].answerChoices.length; j++) {
-      var choiceDiv = $("<div></div>");
-      var choiceInput = $("<input>")
+      var choiceDiv = $("<div>");
       var choiceLabel = $("<label></label>")
+      var choiceInput = $("<input>")
       var choice = questions[i].answerChoices[j];
 
-      choiceDiv.addClass("form-check");
-      choiceInput.attr("name", "10" + i);
+      choiceDiv.addClass("radio");
       choiceInput.attr("type", "radio");
+      choiceInput.attr("name", "10" + i);
       choiceInput.attr("value", j);
-      choiceInput.addClass("with-gap");
       choiceInput.attr("id", "radio" + i + j);
       choiceLabel.attr("for", "radio" + i + j);
 
       choiceLabel.text(choice);
       console.log(questions[i].answerChoices[j]);
-      questionRow.append(choiceDiv, choiceInput, choiceLabel);
+      questionRow.append(choiceDiv);
+      choiceDiv.append(choiceInput, choiceLabel);
     }
   }
 }
 
-//this doesn't work yet, need to correct so that array populates correctly
-$(document).ready(function(){
-  $('#btnGetValue').click(function() {
-    var answerArray = [];
-    for (var i = 0; i < questions.length; i++) {
-      var selValue = $('input[type=radio]:checked').val();
-      answerArray.push(selValue);
-    }  
-    // var selValue = $('input[name=100]:checked').val(); 
-      // $('p').html('<br/>Selected Radio Button Value is : <b>' + selValue + '</b>');
-      console.log(answerArray);
-  });
+// Get the radio button values and calculate the score when the button is clicked
+$("#btnDone").click(function () {
+  var answerArray = [];
+  for (var i = 0; i < questions.length; i++) {
+    var ansValue = $("input[name=10" + i + "]:checked").val();
+    answerArray.push(ansValue);
+  }
+  console.log(answerArray);
+
+  answerKey = [2, 2, 0, 2, 1, 3, 0, 0, 3, 0];
+
+  //Calculate the score by comparing the user's answer array to the answer key array
+  for (var i = 0; i < answerArray.length; i++) {
+    if (answerArray[i] == answerKey[i]) {
+      score++;
+    }
+  }
+  console.log(score);
+
+  stop();
+
+  $("#timeRemaining").hide();
+  $("#timer").hide();
+  $(".questionRow").hide();
+  $("#btnDone").hide();
+
+  var resultsHeader = "YOUR RESULTS"
+  $(".intro").html("<h1>" + resultsHeader + "</h1>");
+
+  var resultsText = "";
+  var image = $("<img>");
+
+  if (score <= 3) {
+    resultsText = "Like Jose's mustache, your results are thin on correct answers..."
+    image.attr("src", "assets/images/JoseCanseco.jpg");
+  } else if (score <= 6) {
+    resultsText = "Like Jose's mustache, your results are thin on correct answers..."
+    image.attr("src", "assets/images/JoseCanseco.jpg");
+  } else if (score <= 9) {
+    resultsText = "Like Jose's mustache, your results are thin on correct answers..."
+    image.attr("src", "assets/images/JoseCanseco.jpg");
+  } else {
+    resultsText = "Like Jose's mustache, your results are thin on correct answers..."
+    image.attr("src", "assets/images/JoseCanseco.jpg");
+  }
+
+$(".resultStatement").html("<p>" + resultsText + "</p>");
+$(".image").append(image);
+
 });
 
 // MAIN PROCESS
 // ==============================================================================
 
-// Calling functions to start the game.
-// renderQuestion();
-// updateScore();
 run();
 testQuestions();
